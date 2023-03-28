41572a84c87f64a6e57410e9d27b29680ebdc6b0
inioluwa14 <63956918+inioluwa14@users.noreply.github.com>
2023-03-28 22:06:16 +0100

    version one

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+function header(){
+    return(
+       <div className="container">
+            <header>
+                <h1 className="title">Welcome to my website!</h1>
+                <nav className="nav-bar">
+                    <ul className="navItem">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+       </div>
+    )
+}
+
+export default header;
---
+++ src/Main.js
@@ -0,0 +1,26 @@
+
+const main = () => {
+    return (
+        <main className="main">
+            <section className="about-section">
+                <h2 className="aboutUs">About Us</h2>
+                <p className="about-us-text">
+                We are a small team of web developers based in the United States. We
+                pecialise in creating modern and responsive websites for businesses
+                and individuals.
+                </p>
+            </section>
+            <section className="second-section">
+                <h2 className="service-title">Our Services</h2>
+                <ul className="service-list">
+                    <li className="service-item">Web Design</li>
+                    <li className="service-item">Web Development</li>
+                    <li className="service-item">E-commerce Solutions</li>
+                    <li className="service-item">Search Engine Optimization (SEO)</li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default main;
